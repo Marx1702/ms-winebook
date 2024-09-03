fdbb80caa4bf5ab591e0b1597d6c935278372bdc
Marcelo Arteaga <marce.arteaga1997@gmail.com>
2024-09-03 20:40:03 -0300

    Service & Controller Rol-Permission
    Se crea el servicio y el controlador para la asignacion de permisos a los roles .Quedan asociados a traves de id .

--- src/services/roles_permissions/roles_permissions.service.ts
+++ src/services/roles_permissions/roles_permissions.service.ts
@@ -1,4 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { prisma_service } from "../../services/prisma/prisma.service";
+import { roles_permissions } from '@prisma/client';
 
 @Injectable()
-export class RolesPermissionsService {}
+export class roles_permissions_service {
+  constructor(private readonly prisma: prisma_service) {}
+
+  // Asignar un permiso a un rol
+  async assignPermissionToRole(rolId: number, permissionId: number): Promise<roles_permissions> {
+    return this.prisma.roles_permissions.create({
+      data: {
+        rolId,
+        permissionId,
+      },
+    });
+  }
+
+  // Obtener todos los permisos asignados a un rol
+  async getPermissionsByRoleId(rolId: number): Promise<roles_permissions[]> {
+    return this.prisma.roles_permissions.findMany({
+      where: {
+        rolId,
+      },
+      include: {
+        permission: true, // Incluir la entidad Permission en la respuesta
+      },
+    });
+  }
+
+  // Eliminar un permiso de un rol
+  async removePermissionFromRole(rolId: number, permissionId: number): Promise<roles_permissions | null> {
+    const rolePermission = await this.prisma.roles_permissions.findFirst({
+      where: {
+        rolId,
+        permissionId,
+      },
+    });
+
+    if (rolePermission) {
+      await this.prisma.roles_permissions.deleteMany({
+        where: {
+          rolId,
+          permissionId,
+        },
+      });
+      return rolePermission;
+    }
+
+    return null;
+  }
+}
