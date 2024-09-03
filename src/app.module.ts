7be4b254a13e760b445ffe1caea0da36847151c5
Marcelo Arteaga <marce.arteaga1997@gmail.com>
2024-09-02 21:46:30 -0300

    Permisison Service And Controller
    Se crea el servicio y el controlador de permisos con sus metodos correspondientes.Fueron testeados todos los metodos.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { user_service } from './services/user/user.service';
-// //import { PermissionService } from './permission/permission.service';
+import { permission_service } from './services/permission/permission.service';
 // import { ActionService } from './action/action.service';
 // import { CompanyService } from './company/company.service';
 // import { EstablishmentService } from './establishment/establishment.service';
@@ -12,10 +12,9 @@ import { users_roles_service } from './services/user_rol/user_rol.service';
 import { prisma_service } from './services/prisma/prisma.service';
 import { user_controller } from './controllers/user/user.controller';
 import { rol_controller } from './controllers/rol/rol.controller';
-//import { PermissionsController } from './permissions/permissions.controller';
 //import { ActionsController } from './actions/actions.controller';
 import { ActionController } from './controllers/action/action.controller';
-import { PermissionController } from './controllers/permission/permission.controller';
+import { permission_controller } from './controllers/permission/permission.controller';
 import { CompanyController } from './controllers/company/company.controller';
 import { EstablishmentController } from './controllers/establishment/establishment.controller';
 import { rol_service } from './services/rol/rol.service';
@@ -24,7 +23,7 @@ import { users_roles_controller } from './controllers/user_rol/user_rol.controll
 
 @Module({
   imports: [],
-  controllers: [AppController, user_controller, rol_controller, ActionController, PermissionController, CompanyController, EstablishmentController, users_roles_controller],
-  providers: [AppService, user_service, prisma_service,rol_service,users_roles_service],
+  controllers: [AppController, user_controller, rol_controller, ActionController, permission_controller, CompanyController, EstablishmentController, users_roles_controller],
+  providers: [AppService, user_service, prisma_service,rol_service,users_roles_service,permission_service],
 })
 export class AppModule {}
